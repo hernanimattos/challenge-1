8cb7b266680c1228298f84396e99ab7ddfce1e3f
Hernani Mattos <hernani.luiz@gmail.com>
2020-08-17 18:32:15 -0300

    challenge 1

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,80 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+const checkIdParams = (req, resp, next) => {
+	const { id } = req.params;
+	const { likes } = req.body;
+
+	if (likes) {
+		return resp.status(400).send({ likes: 0 });
+	}
+	const findIndex = repositories.findIndex((repo) => repo.id == id);
+	if (findIndex < 0) {
+		return resp.status(400).send({ status: 'erro' });
+	}
+	return next();
+};
+
+app.get('/repositories', (request, response) => {
+	return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+	const { url, title, techs } = request.body;
+	const createId = uuid();
+	repositories.push({
+		id: createId,
+		url,
+		title,
+		techs,
+		likes: 0,
+	});
+
+	return response.json({
+		id: createId,
+		url,
+		title,
+		techs,
+		likes: 0,
+	});
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', checkIdParams, (request, response) => {
+	// TODO
+	const { id } = request.params;
+	const { title, techs, url } = request.body;
+
+	const findIndex = repositories.findIndex((repo) => repo.id === id);
+	repositories[findIndex] = {
+		id,
+		title,
+		techs,
+		url,
+	};
+	return response.json({ ...repositories[findIndex] });
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.delete(
+	'/repositories/:id',
+	checkIdParams,
+	checkIdParams,
+	(request, response) => {
+		// TODO
+		const { id } = request.params;
+		const findIndex = repositories.findIndex((repo) => repo.id == id);
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+		repositories.splice(findIndex, 1);
+		return response.status(204).send();
+	},
+);
+
+app.post('/repositories/:id/like', checkIdParams, (request, response) => {
+	const { id } = request.params;
+
+	const findIndex = repositories.findIndex((repo) => repo.id == id);
+
+	const likes = (repositories[findIndex].likes += 1);
+	return response.json({ likes });
 });
 
 module.exports = app;
